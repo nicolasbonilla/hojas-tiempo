085d3b7c51f0f1293c48bf96d6002803db5d1132
Nicolás Bonilla <nicolasbonillavargas@gmail.com>
2023-12-26 20:28:13 -0500

    make it better

--- config/env.js
+++ config/env.js
@@ -18,16 +18,16 @@ const Development = {
 
 const Production = {
         'app':{
-                "port": process.env.PORT,// en producción dejar en 0 o configurar solo si es necesario
-                "urlsb":"", // url backend
+                "port": 8080,
+                "urlsb":"", //url backend
                 "urlsf":"" // url frontend
         },
         'database':{
-                'host': process.env.DB_HOST,
-                'port': process.env.DB_PORT,
-                'database': process.env.DB_NAME,
-                'user': process.env.DB_USER,
-                'password': process.env.DB_PASS
+                'host': "34.173.138.148",
+                'port': "3306",
+                'database': "hojas_tiempo",
+                'user': "admin",
+                'password': "admin"
         } 
 }
 
