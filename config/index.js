50b940c0378fb14cf000d17ecbdcc017a332bf16
Targaryen <leocj@live.com.co>
2024-02-29 12:34:23 -0500

    versión

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ import env from './env.js'
 
 const Config = {
 
-    'version': "1.0.0.0 28-02-2024 10:33",
+    'version': "1.0.0.0 29-02-2024 12:34",
     // development o production
     'status': process.env.ENVIRONMENT || 'development',
 
